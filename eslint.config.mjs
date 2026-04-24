d21ba922dd7600a8ec37fe1f47beda9ee2e4780e
zzz664 <temperis0512@gmail.com>
2026-04-25 03:43:01 +0900

    init(dev-flow): project initialize
    - eslint, prettier config
    - install essential pakages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,15 +10,22 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+      'no-trailing-spaces': ['error'],
+      'prefer-const': 'error',
+      eqeqeq: 'error',
+      curly: 'error',
+      'no-implicit-globals': 'error',
+      'no-var': 'error',
+    },
   },
 ];
 
